b6cdb580ff0ddd62c56f0f823d2d181e98d9b742
Avinash Nath Aita <aviaita4@gmail.com>
2018-06-10 11:34:40 -0700

    Basic API BC interaction working: Few modications

--- Booking.sol
+++ Booking.sol
@@ -5,65 +5,52 @@ contract Booking {
 
   struct Booking_request{
 
-    uint request_id;
-    uint rider_id;
-    uint source;
-    uint destination;
-    uint chosen_quotation_id;
+    uint8 request_id;
+    uint8 rider_id;
+    uint8 source;
+    uint8 destination;
+    uint8 chosen_quotation_id;
 
     bytes32 quotations_head;
-    uint num_quotations;
+    uint8 num_quotations;
 
     mapping (bytes32 => Quotation) quotations;
   }
 
   struct Quotation{
 
-    uint quotation_id;
-    uint driver_id;
-    uint quotation_amount;
+    uint8 quotation_id;
+    uint8 driver_id;
+    uint8 quotation_amount;
 
     bytes32 next;
   }
 
-  uint num_booking_requests;
-  mapping (uint => Booking_request) booking_requests;
+  uint8 public num_booking_requests;
+  mapping (uint8 => Booking_request) public booking_requests;
 
-  function Booking(){}
+  mapping (uint8 => uint8) public test_mapping;
+  
 
-  function createNewRequest(uint request_id, uint rider_id, uint source, uint destination) public{
-
-    num_booking_requests++;
-
-    Booking_request memory br = Booking_request(request_id, rider_id, source, destination, 0, 0, 0);
-    
-    /*
-    br.request_id = request_id;
-    br.rider_id = rider_id;
-    br.source = source;
-    br.destination = destination;
-
-    br.chosen_quotation_id = 0;
-    br.num_quotations = 0;
-    */
-
-    booking_requests[request_id] = br;
+  constructor() public{
+    num_booking_requests = 0;
   }
 
-  function addQuotation(uint request_id, uint quotation_id, uint driver_id, uint quotation_amount) public{
+  function getNumRequests() view public returns (uint8){
+    return num_booking_requests;
+  }
+
+  function createNewRequest(uint8 request_id, uint8 rider_id, uint8 source, uint8 destination) public{
+    num_booking_requests++;
+
+    booking_requests[request_id] = Booking_request(request_id, rider_id, source, destination, 0, 0, 0);
+  }
+
+  function addQuotation(uint8 request_id, uint8 quotation_id, uint8 driver_id, uint8 quotation_amount) public{
 
     Booking_request storage br = booking_requests[request_id];
     
     Quotation memory quotation = Quotation(quotation_id, driver_id, quotation_amount, br.quotations_head);
-    
-    /*
-    quotation.next = br.quotations_head;
-
-    quotation.quotation_id = quotation_id;
-    quotation.driver_id = driver_id;
-    quotation.quotation_amount = quotation_amount;
-    */
-
 
     bytes32 id = sha3(quotation.quotation_id, quotation.driver_id, quotation.quotation_amount);
     br.quotations[id] = quotation;
@@ -72,14 +59,14 @@ contract Booking {
     br.num_quotations++;
   }
 
-  function getAllQuotations(uint request_id) public returns (bytes32[]){
+  function getAllQuotations(uint8 request_id) view public returns (bytes32[]){
     
     Booking_request storage br = booking_requests[request_id];
     
     bytes32[] memory quotation_map_ids = new bytes32[](br.num_quotations);
     bytes32 current = br.quotations_head;
 
-    for(uint i = 0; i < br.num_quotations; i++){
+    for(uint8 i = 0; i < br.num_quotations; i++){
       quotation_map_ids[i] = current;
       current = br.quotations[current].next;
     }
@@ -87,13 +74,15 @@ contract Booking {
     return quotation_map_ids;
   }
 
-  function getQuotationDetails(uint request_id, bytes32 quotation_map_id) public returns (uint, uint, uint){
+
+  function getQuotationDetails(uint8 request_id, bytes32 quotation_map_id) view public returns (uint8, uint8, uint8){
     Booking_request storage br = booking_requests[request_id];
     Quotation memory quotation = br.quotations[quotation_map_id];
     return (quotation.quotation_id, quotation.driver_id, quotation.quotation_amount);
   }
 
-  function selectQuotation(uint request_id, uint quotation_id) public{
+
+  function selectQuotation(uint8 request_id, uint8 quotation_id) public{
     booking_requests[request_id].chosen_quotation_id = quotation_id;
   }
   
--- node_script.js
+++ node_script.js
@@ -4,6 +4,11 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var bodyParser  = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 code = fs.readFileSync('Booking.sol').toString();
 solc = require('solc');
@@ -19,49 +24,85 @@ web3.eth.defaultAccount = web3.eth.accounts[0]
 //personal.unlockAccount(web3.eth.defaultAccount)
 
 deployedContract = BookingContract.new({data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
-contractInstance = BookingContract.at(deployedContract.address);
+
+//deployedContract.address === undefined
+//while(deployedContract.address === undefined){ console.log("test");}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+// Usage!
+sleep(500).then(() => {
+    
+   console.log("Contract deployed successfully!" + (deployedContract.address !== undefined));
 
 
-// This responds with "Hello World" on the homepage
-app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
-})
+   contractInstance = BookingContract.at(deployedContract.address);
 
-app.post('/createNewRequest', function (req, res) {
-   console.log("Creating brand new request!");
+   // #defines
 
-   //console.log(req.route.stack);
-
-   contractInstance.createNewRequest(req.headers['request_id'], req.headers['rider_id'], req.headers['source'], req.headers['destination']);
-
-   //contractInstance.createNewRequest(1, 10, 12345, 54321);
-
-   res.send('New Request Created');
-})
-
-app.post('/addQuotation', function (req, res) {
-   console.log("Creating quotation for request");
-
-   contractInstance.addQuotation(req.param('request_id'), req.param('quotation_id'), req.param('driver_id'), req.param('uint quotation_amount'));
-
-   res.send('Created a quotation!');
-})
+   max_gas_allowed = 110000;
 
 
-app.get('/list_', function (req, res) {
-   console.log("Got a GET request for /list_user");
-   res.send('Page Listing');
-})
+   // This responds with "Hello World" on the homepage
+   app.get('/', function (req, res) {
+      console.log("Got a GET request for the homepage");
+      res.send('Hello GET');
+   })
 
+   app.get('/getTotalNumReqs', function (req, res) {
+      num_reqs = contractInstance.getNumRequests();
+      res.send('Toral Requests: ' + num_reqs);
+   })
 
-var server = app.listen(8081, function () {
+   app.post('/createNewRequest', function (req, res) {
+      console.log("Creating brand new request!");
 
-   var host = server.address().address
-   var port = server.address().port
+      contractInstance.createNewRequest.sendTransaction(req.body.request_id, req.body.rider_id, req.body.source, req.body.destination, {gas:max_gas_allowed});
 
-   console.log("Booking test app listening at http://%s:%s", host, port)
-})
+      res.send('New Request Created');
+   })
+
+   app.post('/addQuotation', function (req, res) {
+      console.log("Creating quotation for request");
+
+      contractInstance.addQuotation.sendTransaction(req.body.request_id, req.body.quotation_id, req.body.driver_id, req.body.quotation_amount, {gas:max_gas_allowed});
+
+      res.send('Created a quotation!');
+   })
+
+   app.get('/getAllQuotationDetails', function (req, res) {
+      console.log("Getting all quotation details of a request");
+
+      result = [];
+      console.log(req);
+      console.log(req.query.request_id);
+      quotation_map_ids = contractInstance.getAllQuotations(req.query.request_id);
+      for (var i in quotation_map_ids) {
+        temp = contractInstance.getQuotationDetails(req.query.request_id, quotation_map_ids[i]);
+        json_temp = {
+            'Quatation ID' : temp[0],
+            'Driver ID' : temp[1],
+            'Amount':temp[2]
+        }
+        result.push(json_temp);
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(result, null, 3));
+   })
+
+   var server = app.listen(8081, function () {
+
+      var host = server.address().address
+      var port = server.address().port
+
+      console.log("Booking test app listening at http://%s:%s", host, port)
+   })
+
+});
+
 
 
 
